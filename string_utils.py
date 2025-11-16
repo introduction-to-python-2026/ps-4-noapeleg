77de8843748500b609eeeeebfd37c704295f4926
noapeleg <noapeleg2002@gmail.com>
2025-11-16 18:03:33 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,31 +1,24 @@
+def split_at_first_digit(formula):
+    if not formula:
+        return "", 1
+    for idx, char in enumerate(formula):
+        if char.isdigit():
+            start = idx
+            end = idx
+            while end < len(formula) and formula[end].isdigit():
+                end += 1
+            return formula[:start], int(formula[start:end])
+    return formula, 1
+
 def split_before_each_uppercases(formula):
     if not formula:
-        return [], 1 
-    number = 1
-    i = len(formula) - 1
-    while i >= 0 and formula[i].isdigit():
-        i -= 1
-    if i < len(formula) - 1:
-        number = int(formula[i+1:])
-        formula = formula[:i+1]
-    result = []
+        return []
+
+    split_formula = []
     start = 0
     for i in range(1, len(formula)):
         if formula[i].isupper():
-            result.append(formula[start:i])
+            split_formula.append(formula[start:i])
             start = i
-    if start < len(formula):
-        result.append(formula[start:])  
-    return result, number
-
-def split_at_first_digit(formula):
-    if not formula:
-        return "",1
-    for idx,char in enumerate(formula):
-        if char.isdigit():
-            start=idx
-            end=idx
-            while end<len(formula) and formula[end].isdigit():
-                end+=1
-            return formula[:start],int(formula[start:end])
-    return formula,1
+    split_formula.append(formula[start:])
+    return split_formula
